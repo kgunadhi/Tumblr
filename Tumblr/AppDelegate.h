4c70291f6b234cdeb81a2997adef76fff1501212
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-06-25 08:40:03 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Kaitlyn Gunadhi on 6/25/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Kaitlyn Gunadhi on 6/25/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
