fdfadb312fb656408fa8fc5e2524367ae0046095
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-08-14 07:37:11 -0700

    Initial commit

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Kaitlyn Gunadhi on 6/25/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
